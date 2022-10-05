6b804b871ba60eea2c58694d1fce9715d5e23835
Alejandro Higuerey <ahiguerey.3662@unimar.edu.ve>
2022-10-05 09:08:47 -0400

    eliminando archivo1

--- archivo1.pas
+++
@@ -1,6 +0,0 @@
-program archivo1;
-
-begin
-writeln ('no me esperaba crear otro archivo pero bueno, todo por el tutorial, papi este tuto me esta troleando feo, ayuda tercera modificacion');
-readln ();
-end.
