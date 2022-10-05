d7ca026af53a0fc3285dcefa9f1fc184bfaa23a5
Alejandro Higuerey <ahiguerey.3662@unimar.edu.ve>
2022-10-05 09:04:56 -0400

    explicando como he modificado ya por tercera vez

--- archivo1.pas
+++ archivo1.pas
@@ -1,6 +1,6 @@
 program archivo1;
 
 begin
-writeln ('no me esperaba crear otro archivo pero bueno, todo por el tutorial, papi este tuto me esta troleando feo');
+writeln ('no me esperaba crear otro archivo pero bueno, todo por el tutorial, papi este tuto me esta troleando feo, ayuda tercera modificacion');
 readln ();
 end.
