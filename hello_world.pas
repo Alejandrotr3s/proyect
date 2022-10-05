f7f4df919197251db4a5cbfbb474c67d415a39c7
Alejandro Higuerey <ahiguerey.3662@unimar.edu.ve>
2022-10-05 09:26:27 -0400

    comprobando status

---
+++ hello_world.pas
@@ -0,0 +1,6 @@
+program hello_world;
+
+begin
+    writeln('hello world');
+    readln();
+end.
--- hola_mundo.pas
+++ hola_mundo.pas
@@ -1,6 +1,7 @@
-program archivo;
+program hola_mundo;
 
 begin
     writeln('hola mundo');
+    WriteLn('que tal mundo');
     ReadLn();
 end.
